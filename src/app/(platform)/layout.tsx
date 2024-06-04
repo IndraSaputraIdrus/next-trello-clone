41f7b762f98cd43bf4d03a6f13d8cd0b7ee3d3b6
pcpeace5 <pcpeace5@gmail.com>
2024-06-04 16:09:24 +0000

    feat: init auth with clerk

---
+++ src/app/(platform)/layout.tsx
@@ -0,0 +1,11 @@
+import { ClerkProvider } from "@clerk/nextjs";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const PlatformLayout = ({ children }: Props) => {
+  return <ClerkProvider>{children}</ClerkProvider>;
+};
+
+export default PlatformLayout;
